48121e28111809d5161146ffb4d125a8a64b1dcf
codeacme17 <vir_kings@qq.com>
2022-11-30 14:04:02 +0800

    206 @2022.11.30

---
+++ Leetcode/Easy/121. Best Time to Buy and Sell Stock.ts
@@ -0,0 +1,11 @@
+/**
+ *  121. 买卖股票的最佳时机
+ *  https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+ *
+ */
+
+function maxProfit(prices: number[]): number {
+  //
+
+  return 0;
+}
---
+++ Leetcode/Easy/206. Reverse Linked List.ts
@@ -0,0 +1,37 @@
+/**
+ *  206. 反转链表
+ *  https://leetcode.cn/problems/reverse-linked-list/
+ *
+ *  思路：
+ *  需要准备两个指针分别指向当前节点cur和上一个节点pre，和一个备用链表temp
+ *  当cur存在时进入循环
+ *  1. 将cur.next存入到temp中，这样是避免失去原始head中的链表关系
+ *  2. 将cur.next指向pre
+ *  3. 将pre移动到cur
+ *  4. 将temp存入cur
+ *
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let pre: ListNode | null = null;
+  let cur: ListNode | null = head;
+  let temp: ListNode | null;
+
+  while (cur) {
+    temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp;
+  }
+
+  return pre;
+}
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
