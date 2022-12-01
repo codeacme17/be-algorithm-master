121da78d40bb18de63bffba58927b58dc91d645a
codeacme17 <vir_kings@qq.com>
2022-12-01 10:57:42 +0800

    53 @2022.12.1

---
+++ Leetcode/Middle/53. Maximum Subarray.ts
@@ -0,0 +1,27 @@
+/**
+ *  53. 最大子数组和
+ *  https://leetcode.cn/problems/maximum-subarray/
+ *
+ *  思路：
+ *  动态规划，dp[i] 表示 0->i 的和
+ *  当 dp[i-1] <= 0 时，可以直接将之前的 sum 更改为 i
+ *  这是因为，如果 dp[i-1] <= 0 时与 i 相加都不会比 i 本身大
+ *  最后定义一个 max 来维护每次遍历的最大值
+ *
+ */
+
+function maxSubArray(nums: number[]): number {
+  let sum = 0;
+  let max = nums[0];
+
+  for (const num of nums) {
+    if (sum > 0) sum += num;
+    else sum = num;
+
+    max = Math.max(sum, max);
+  }
+
+  return max;
+}
+
+console.log(maxSubArray([1, 3, -2, 3]));
