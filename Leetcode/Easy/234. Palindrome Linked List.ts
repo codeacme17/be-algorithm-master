737dd60c457b6a379cb7c652d9d2f9884ba5ff64
codeacme17 <vir_kings@qq.com>
2022-12-08 18:00:41 +0800

    234 @2022.12.8

---
+++ Leetcode/Easy/234. Palindrome Linked List.ts
@@ -0,0 +1,18 @@
+/**
+ *  234. 回文链表
+ *  https://leetcode.com/problems/palindrome-linked-list/
+ *
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+  return false;
+}
