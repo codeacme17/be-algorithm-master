165b9ff305f59e32578fe9e59a2d24f5f6ccf55d
codeacme17 <vir_kings@qq.com>
2022-12-09 17:16:39 +0800

    83! @2022.12.9

---
+++ Leetcode/Easy/83. Remove Duplicates from Sorted List.ts
@@ -0,0 +1,16 @@
+/**
+ *  83. 删除排序链表中的重复元素
+ *  https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ *
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {}
