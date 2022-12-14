b67343b53c46a94ebf832317a87c195dd17aa825
codeacme17 <vir_kings@qq.com>
2022-12-14 16:20:49 +0800

    167 @2022.12.14

--- Leetcode/Medium/167. Two Sum II - Input Array Is Sorted.ts
+++ Leetcode/Medium/167. Two Sum II - Input Array Is Sorted.ts
@@ -2,6 +2,15 @@
  *  167. 两数之和 II - 输入有序数组
  *  https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
  *
+ *  思路：
+ *  左右指针，分别指向数组两端
+ *  当 left < right 时，进入循环
+ *  如果两指针所指值相加与目标相同，则返回结果
+ *  因为条件给的是 “non-decreasing array" 所以可以保证左指针的值一定小于右指针
+ *  所以就可以判断：
+ *  当和小于目标是，将左指针向右移，即增加和的大小
+ *  反之则右指针向左移
+ *
  */
 
 function twoSum(numbers: number[], target: number): number[] {
