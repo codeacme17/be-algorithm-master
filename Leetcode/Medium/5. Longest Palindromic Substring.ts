f6627f62e7a300c57c204b6af26299c7c45a33c1
codeacme17 <vir_kings@qq.com>
2022-12-17 15:02:52 +0800

    5 @2022.12.17

--- Leetcode/Medium/5. Longest Palindromic Substring.ts
+++ Leetcode/Medium/5. Longest Palindromic Substring.ts
@@ -6,12 +6,24 @@
  */
 
 function longestPalindrome(s: string): string {
-  const stack: string[] = [];
+  let res = "";
 
-  for (let i of s) {
+  for (let i = 0; i < s.length; i++) {
+    let s1 = palindrome(s, i, i);
+    let s2 = palindrome(s, i, i + 1);
+    res = res.length > s1.length ? res : s1;
+    res = res.length > s2.length ? res : s2;
   }
 
-  return "";
+  return res;
+}
+
+function palindrome(s: string, l: number, r: number): string {
+  while (l >= 0 && r < s.length && s[l] === s[r]) {
+    l--;
+    r++;
+  }
+  return s.substring(l + 1, r);
 }
 
 console.log(longestPalindrome("babad"));
