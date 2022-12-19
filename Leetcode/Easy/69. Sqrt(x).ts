8ce35e337e8bc5f64dcd68c5565f48841d47378b
codeacme17 <vir_kings@qq.com>
2022-12-19 17:09:37 +0800

    69 @2022.12.19

---
+++ Leetcode/Easy/69. Sqrt(x).ts
@@ -0,0 +1,23 @@
+/**
+ *  69. x 的平方根
+ *  https://leetcode.com/problems/sqrtx/
+ *
+ */
+
+function mySqrt(x: number): number {
+  let l = 1;
+  let r = x;
+
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+
+    if (mid <= x / mid) {
+      if (mid + 1 > x / (mid + 1)) return mid;
+      else l = mid + 1;
+    } else r = mid - 1;
+  }
+
+  return 0;
+}
+
+console.log(mySqrt(1));
