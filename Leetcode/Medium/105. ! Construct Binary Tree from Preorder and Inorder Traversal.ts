36a1859b81699051496aeb29b6ff993e05730931
codeacme17 <vir_kings@qq.com>
2022-12-29 16:17:09 +0800

    105 @2022.12.29

--- Leetcode/Medium/105. ! Construct Binary Tree from Preorder and Inorder Traversal.ts
+++ Leetcode/Medium/105. ! Construct Binary Tree from Preorder and Inorder Traversal.ts
@@ -5,7 +5,25 @@
  *  思路：
  *  preorder: 1, 2, 5, 4, 6, 7, 3, 8, 9
  *  inorder:  5, 2, 6, 4, 7, 1, 9, 3, 9
+ *  可以判断出，每次递归的时候，preorder 的第一个元素就是当前的根节点 - preorder[preStart]
+ *  而 inorder 之中，与该根节点值相同的元素的左侧代表的是该根节点下的左树，同理右边则是右树
  *
+ *  问题：
+ *  1.
+ *  所以第一个需要解决的问题就是从 inorder 中获取到该根节点的下标
+ *  我们可以在进入函数的时候创建一个 Map 来存储 inorder 的各元素和其下标
+ *  Map <key: element，value: index>
+ *  2.
+ *  拿到 inorder 中的根节点后，就需要 ’拆分‘ 两条数组，
+ *  将其分为左树和右树，从而进行递归
+ *  inorder 很容易进行拆分，因为 index 左侧的就是左树，右侧的就是右树
+ *  难点在于 preorder 的拆分
+ *  3.
+ *  如果想要拆分 preorder 树，需要确定左树的长度 leftSize，
+ *  这样 [preStart + 1 ... leftSize - 1] 就代表 preorder 上的左树
+ *  而 [preStart + 1 + leftSize ... preEnd] 就代表 preorder 上的右树
+ *
+ *  如此一来就可以将 preorder 和 inorder 中的左右树进行递归操作，从而获取结果
  *
  */
 
