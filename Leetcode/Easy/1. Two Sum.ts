ed6108c1bbbf11e0963f3f8a628533bc166ee007
codeacme17 <vir_kings@qq.com>
2023-01-29 10:21:33 +0800

    20 @2023.1.29

--- Leetcode/Easy/1. Two Sum.ts
+++ Leetcode/Easy/1. Two Sum.ts
@@ -9,22 +9,20 @@
  *  如果 Hash Table 中存在该差值，则表示有这两数，返回数组
  *  如果不存在，则将 num[i] 作为 key，将 i 作为 value 存入 Hash 中
  *
- *  Review:
- *  2022.12.6 Good
  *
  */
 
 function twoSum(nums: number[], target: number): number[] {
-  const map: Map<number, number> = new Map();
+  const map: Map<number, number> = new Map()
 
   for (let i = 0; i < nums.length; i++) {
-    const comp = target - nums[i];
+    const comp = target - nums[i]
 
-    if (map.has(comp)) return [map.get(comp) as number, i];
-    else map.set(nums[i], i);
+    if (map.has(comp)) return [map.get(comp) as number, i]
+    else map.set(nums[i], i)
   }
 
-  return [];
+  return []
 }
 
-twoSum([2, 11, 7, 15], 9);
+twoSum([2, 11, 7, 15], 9)
