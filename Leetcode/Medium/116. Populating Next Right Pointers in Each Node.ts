494ee77bfd6d50a960bd6eb2b9b9d744b7d23f24
codeacme17 <vir_kings@qq.com>
2023-02-10 20:44:18 +0800

    116 @2023.2.10

--- Leetcode/Medium/116. Populating Next Right Pointers in Each Node.ts
+++ Leetcode/Medium/116. Populating Next Right Pointers in Each Node.ts
@@ -5,29 +5,29 @@
  */
 
 class MNode {
-  val: number;
-  left: MNode | null;
-  right: MNode | null;
-  next: MNode | null;
+  val: number
+  left: MNode | null
+  right: MNode | null
+  next: MNode | null
   constructor(val?: number, left?: MNode, right?: MNode, next?: MNode) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-    this.next = next === undefined ? null : next;
+    this.val = val === undefined ? 0 : val
+    this.left = left === undefined ? null : left
+    this.right = right === undefined ? null : right
+    this.next = next === undefined ? null : next
   }
 }
 
 function connect(root: MNode | null): MNode | null {
-  if (!root) return null;
-  traverse(root.left, root.right);
-  return root;
+  if (!root) return null
+  traverse(root.left, root.right)
+  return root
 
   function traverse(node1: MNode | null, node2: MNode | null): void {
-    if (!node1 || !node2) return;
-    node1.next = node2;
+    if (!node1 || !node2) return
+    node1.next = node2
 
-    traverse(node1.left, node1.right);
-    traverse(node2.left, node2.right);
-    traverse(node1.right, node2.left);
+    traverse(node1.left, node1.right)
+    traverse(node2.left, node2.right)
+    traverse(node1.right, node2.left)
   }
 }
