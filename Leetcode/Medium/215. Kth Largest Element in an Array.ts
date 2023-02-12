4e14e74a27c1cc24e95364be4efa6f764b54b209
codeacme17 <vir_kings@qq.com>
2023-02-12 14:09:56 +0800

    215 @2023.2.12

--- Leetcode/Medium/215. Kth Largest Element in an Array.ts
+++ Leetcode/Medium/215. Kth Largest Element in an Array.ts
@@ -2,11 +2,42 @@
  *  215. 数组中的第K个最大元素
  *  https://leetcode.com/problems/kth-largest-element-in-an-array/
  *
+ *  ref: https://www.bilibili.com/video/BV1at411T75o/?spm_id_from=333.337.search-card.all.click
  *
  */
 
 function findKthLargest(nums: number[], k: number): number {
-  return nums.sort((a, b) => a - b)[k - 1];
+  sort(nums, 0, nums.length - 1)
+  return nums[nums.length - k]
 }
 
-console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+function sort(nums: number[], lo: number, hi: number): void {
+  if (lo >= hi) return
+  let p = partition(nums, lo, hi)
+  sort(nums, lo, p - 1)
+  sort(nums, p + 1, hi)
+}
+
+function partition(nums: number[], lo: number, hi: number): number {
+  let pivot = nums[lo]
+  let i = lo + 1
+  let j = hi
+
+  while (i <= j) {
+    while (i < hi && nums[i] <= pivot) i++
+    while (j > lo && nums[j] > pivot) j--
+    if (i >= j) break
+    swap(nums, i, j)
+  }
+
+  swap(nums, lo, j)
+  return j
+}
+
+function swap(nums: number[], lo: number, hi: number): void {
+  let temp = nums[lo]
+  nums[lo] = nums[hi]
+  nums[hi] = temp
+}
+
+findKthLargest([3, 1, 5, 6, 2], 0)
