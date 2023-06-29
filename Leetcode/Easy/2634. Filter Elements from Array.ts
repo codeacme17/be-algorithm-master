68262ad55556abe86c23e74d878d0d7839b42c87
leyoonafr <vir_kings@qq.com>
2023-06-29 16:08:52 +0800

    add: 2634

---
+++ Leetcode/Easy/2634. Filter Elements from Array.ts
@@ -0,0 +1,18 @@
+/**
+ *  2634. Filter Elements from Array
+ *  https://leetcode.com/problems/filter-elements-from-array/
+ *
+ */
+
+function filter(
+  arr: number[],
+  fn: (n: number, i: number) => any
+): number[] {
+  const res: number[] = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) res.push(arr[i])
+  }
+
+  return res
+}
